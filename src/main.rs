0308f5fd2ae304a549b23b6bd046f1f5e0ca1cc2
Bart Massey <bart@cs.pdx.edu>
2019-09-04 03:39:54 -0700

    took suggestions from cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 //! *Hunt the Wumpus* reimplementation in Rust.
 
+use rand::prelude::*;
 use std::error::Error;
 use std::io::{self, Write};
 use std::process::exit;
-use rand::prelude::*;
 
 /// Help message.
 const HELP: &str = "\
@@ -119,14 +119,29 @@ struct Maze {
 }
 
 impl Maze {
-
     // List of adjacencies used to wire up the dodecahedron.
     // https://stackoverflow.com/a/44096541/364875
     const ADJS: [[usize; 3]; 20] = [
-        [1, 4, 7], [0, 2, 9], [1, 3, 11], [2, 4, 13], [0, 3, 5],
-        [4, 6, 14], [5, 7, 16], [0, 6, 8], [7, 9, 17], [1, 8, 10],
-        [9, 11, 18], [2, 10, 12], [11, 13, 19], [3, 12, 14], [5, 13, 15],
-        [14, 16, 19], [6, 15, 17], [8, 16, 18], [10, 17, 19], [12, 15, 18],
+        [1, 4, 7],
+        [0, 2, 9],
+        [1, 3, 11],
+        [2, 4, 13],
+        [0, 3, 5],
+        [4, 6, 14],
+        [5, 7, 16],
+        [0, 6, 8],
+        [7, 9, 17],
+        [1, 8, 10],
+        [9, 11, 18],
+        [2, 10, 12],
+        [11, 13, 19],
+        [3, 12, 14],
+        [5, 13, 15],
+        [14, 16, 19],
+        [6, 15, 17],
+        [8, 16, 18],
+        [10, 17, 19],
+        [12, 15, 18],
     ];
 
     /// A maze is made up of rooms connected as a dodecahedron
@@ -199,32 +214,39 @@ impl Maze {
         }
 
         if let Some(_) = self.is_wumpus_nearby(room) {
-            desc_lines.push("You smell something terrible nearby.".to_string());
+            desc_lines.push(
+                "You smell something terrible nearby.".to_string(),
+            );
         }
 
-        let neighbors: Vec<String> = self.rooms[room].neighbors
+        let neighbors: Vec<String> = self.rooms[room]
+            .neighbors
             .iter()
             .map(|i| i.to_string())
             .collect();
 
-        desc_lines.push(format!(
-            "Exits go to: {}",
-            neighbors.join(", "),
-        ));
+        desc_lines
+            .push(format!("Exits go to: {}", neighbors.join(", "),));
 
         desc_lines.join("\n")
     }
 
     /// Adjacent room contains a non-wumpus danger.
-    fn is_danger_nearby(&self, room: usize, danger: RoomContents) -> bool {
-        self.rooms[room].neighbors
+    fn is_danger_nearby(
+        &self,
+        room: usize,
+        danger: RoomContents,
+    ) -> bool {
+        self.rooms[room]
+            .neighbors
             .iter()
             .any(|&n| self.rooms[n].contents == danger)
     }
 
     /// Specific adjacent room contains the Wumpus.
     fn is_wumpus_nearby(&self, room: usize) -> Option<usize> {
-        self.rooms[room].neighbors
+        self.rooms[room]
+            .neighbors
             .iter()
             .cloned()
             .find(|&n| self.rooms[n].wumpus)
@@ -232,14 +254,21 @@ impl Maze {
 
     /// Index of neighboring room given by user
     /// `destination`, else an error message.
-    fn parse_room(&self, destination: &str, current_room: usize) -> Result<usize, String> {
-        let dest: usize = destination
-            .trim()
-            .parse()
-            .map_err(|e: std::num::ParseIntError| e.description().to_string())?;
+    fn parse_room(
+        &self,
+        destination: &str,
+        current_room: usize,
+    ) -> Result<usize, String> {
+        let dest: usize = destination.trim().parse().map_err(
+            |e: std::num::ParseIntError| e.description().to_string(),
+        )?;
 
         // Check that the given destination is the id of a linked room.
-        if self.rooms[current_room].neighbors.iter().all(|&r| r != dest) {
+        if self.rooms[current_room]
+            .neighbors
+            .iter()
+            .all(|&r| r != dest)
+        {
             return Err("room is not a neighbor".to_string());
         }
 
@@ -276,107 +305,116 @@ fn main() {
     loop {
         prompt();
         let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Cannot read from stdin");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Cannot read from stdin");
         let input: &str = &input.trim().to_lowercase();
 
         match status {
-            Quitting => {
-                match input {
-                    "y" => {
-                        println!("Goodbye, braveheart!");
-                        exit(0);
-                    }
-                    "n" => {
-                        println!("Good. the Wumpus is looking for you!");
-                        status = Normal;
-                    }
-                    _ => println!("That doesn't make any sense")
+            Quitting => match input {
+                "y" => {
+                    println!("Goodbye, braveheart!");
+                    exit(0);
                 }
-            }
-            Moving => match maze.parse_room(input, player.room) {
-                Ok(room) => {
-                    if maze.rooms[room].wumpus {
-                        println!("The wumpus ate you up!");
-                        println!("GAME OVER");
-                        exit(0);
-                    } else if maze.rooms[room].contents == Pit {
-                        println!("You fall into a bottomless pit!");
-                        println!("GAME OVER");
-                        exit(0);
-                    } else if maze.rooms[room].contents == Bat {
-                        println!("The bats whisk you away!");
-                        player.room = maze.rng.gen_range(0, maze.rooms.len());
-                    } else {
-                        player.room = room;
-                    }
-
+                "n" => {
+                    println!("Good. the Wumpus is looking for you!");
                     status = Normal;
-                    describe(&maze, &player);
-                },
-                Err(e) => {
-                    println!("There was a problem with your directions: {}", e);
-                    println!("Where do you want to go?");
                 }
-            }
-            Shooting => match maze.parse_room(input, player.room) {
-                Ok(room) =>  {
-                    if maze.rooms[room].wumpus {
-                        println!("YOU KILLED THE WUMPUS! GOOD JOB, BUDDY!!!");
-                        exit(0);
-                    }
-
-                    if let Some(wumpus_room) = maze.is_wumpus_nearby(room) {
-                        // 75% chances of waking up the wumpus that would go into another room
-                        if maze.rng.gen::<f32>() <= WAKE_WUMPUS_PROB {
-                            let new_wumpus_room =
-                                *maze.rooms[wumpus_room].neighbors.choose(&mut maze.rng)
-                                .unwrap();
-
-                            if new_wumpus_room == player.room {
-                                println!("You woke up the wumpus and he ate you!");
-                                println!("GAME OVER");
-                                exit(1);
-                            }
-
-                            maze.rooms[wumpus_room].wumpus = false;
-                            maze.rooms[new_wumpus_room].wumpus = true;
-                            println!("You heard a rumbling in a nearby cavern.");
-                        }
-                    }
-
-                    player.arrows -= 1;
-                    if  player.arrows == 0 {
-                        println!("You ran out of arrows.");
-                        println!("GAME OVER");
-                        exit(1);
-                    }
-
-                    status = Normal;
-                    describe(&maze, &player);
-                },
-                Err(e) => {
-                    println!("There was a problem with your directions: {}", e);
-                    println!("Where do you want to shoot?");
-                },
+                _ => println!("That doesn't make any sense"),
             },
-            Normal => {
-                match input {
-                    "h" => println!("{}", HELP),
-                    "q" => {
-                        println!("Are you so easily scared? [y/n]");
-                        status = Quitting;
+            Moving => {
+                match maze.parse_room(input, player.room) {
+                    Ok(room) => {
+                        if maze.rooms[room].wumpus {
+                            println!("The wumpus ate you up!");
+                            println!("GAME OVER");
+                            exit(0);
+                        } else if maze.rooms[room].contents == Pit {
+                            println!("You fall into a bottomless pit!");
+                            println!("GAME OVER");
+                            exit(0);
+                        } else if maze.rooms[room].contents == Bat {
+                            println!("The bats whisk you away!");
+                            player.room =
+                                maze.rng.gen_range(0, maze.rooms.len());
+                        } else {
+                            player.room = room;
+                        }
+
+                        status = Normal;
+                        describe(&maze, &player);
                     }
-                    "m" => {
-                        println!("Where?");
-                        status = Moving;
+                    Err(e) => {
+                        println!("There was a problem with your directions: {}", e);
+                        println!("Where do you want to go?");
                     }
-                    "s" => {
-                        println!("Where?");
-                        status = Shooting;
-                    }
-                    _ => println!("That doesn't make any sense")
                 }
             }
+            Shooting => {
+                match maze.parse_room(input, player.room) {
+                    Ok(room) => {
+                        if maze.rooms[room].wumpus {
+                            println!("YOU KILLED THE WUMPUS! GOOD JOB, BUDDY!!!");
+                            exit(0);
+                        }
+
+                        if let Some(wumpus_room) =
+                            maze.is_wumpus_nearby(room)
+                        {
+                            // 75% chances of waking up the wumpus that would go into another room
+                            if maze.rng.gen::<f32>() <= WAKE_WUMPUS_PROB
+                            {
+                                let new_wumpus_room = *maze.rooms
+                                    [wumpus_room]
+                                    .neighbors
+                                    .choose(&mut maze.rng)
+                                    .unwrap();
+
+                                if new_wumpus_room == player.room {
+                                    println!("You woke up the wumpus and he ate you!");
+                                    println!("GAME OVER");
+                                    exit(1);
+                                }
+
+                                maze.rooms[wumpus_room].wumpus = false;
+                                maze.rooms[new_wumpus_room].wumpus =
+                                    true;
+                                println!("You heard a rumbling in a nearby cavern.");
+                            }
+                        }
+
+                        player.arrows -= 1;
+                        if player.arrows == 0 {
+                            println!("You ran out of arrows.");
+                            println!("GAME OVER");
+                            exit(1);
+                        }
+
+                        status = Normal;
+                        describe(&maze, &player);
+                    }
+                    Err(e) => {
+                        println!("There was a problem with your directions: {}", e);
+                        println!("Where do you want to shoot?");
+                    }
+                }
+            }
+            Normal => match input {
+                "h" => println!("{}", HELP),
+                "q" => {
+                    println!("Are you so easily scared? [y/n]");
+                    status = Quitting;
+                }
+                "m" => {
+                    println!("Where?");
+                    status = Moving;
+                }
+                "s" => {
+                    println!("Where?");
+                    status = Shooting;
+                }
+                _ => println!("That doesn't make any sense"),
+            },
         }
     }
 }
